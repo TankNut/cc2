897b11be7e0a9d6e9d93aedc2b8a01df2e75b2c3
Hoplite570 <hoplite570@pm.me>
2024-11-13 17:56:15 +0000

    Remove terminator player_manager setup

--- gamemode/sh_playerclass.lua
+++ gamemode/sh_playerclass.lua
@@ -19,9 +19,3 @@ function PLAYER:FinishMove(move)
 end
 
 player_manager.RegisterClass("player_cc", PLAYER, "player_sandbox")
-
-player_manager.AddValidModel("t600_cc", "models/tnb/skynet/t600.mdl")
-player_manager.AddValidModel("t800_cc", "models/tnb/skynet/t800.mdl")
-
-player_manager.AddValidHands("t600_cc", "models/tnb/trpweapons/c_arms_t600.mdl", 0, "0000000")
-player_manager.AddValidHands("t800_cc", "models/tnb/trpweapons/c_arms_t800.mdl", 0, "0000000")
