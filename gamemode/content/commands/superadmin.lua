163379f90bdcdd024703e0049ddf21d62869694b
TankNut <m.vlasblom@live.nl>
2024-11-27 03:10:42 +0100

    Nitpicks some console command stuff

--- gamemode/content/commands/superadmin.lua
+++ gamemode/content/commands/superadmin.lua
@@ -1,25 +1,28 @@
 local setUserGroup = console.AddCommand("rpa_setusergroup", function(ply, target, usergroup)
 	if IsValid(ply) and (usergroup == "superadmin" or usergroup == "developer") then
-		console.PrintError(ply, "Elevated access must be set from the server console")
+		console.Feedback(ply, "ERROR", "Elevated access must be set from the server console")
 
 		return
 	end
 
 	target:SetUserGroup(usergroup)
 
-	console.PrintMessage(ply, "You've set %s's usergroup to %s", target, usergroup)
-	console.PrintMessage(target, "%s has set your usergroup to %s", ply, usergroup)
+	console.Feedback(ply, "WARNING", "You've set %s's usergroup to %s", target, usergroup)
+	console.Feedback(target, "WARNING", "%s has set your usergroup to %s", ply, usergroup)
 
 	-- TODO: Log this, one of the logging system is setup.
 end)
-setUserGroup:SetAccess(console.IsSuperAdmin)
-setUserGroup:SetExecutionContext(console.Server)
+
 setUserGroup:SetDescription("Updates a player's assigned permission group")
+setUserGroup:SetExecutionContext(console.Server)
+setUserGroup:SetAccess(console.IsSuperAdmin)
+
 setUserGroup:AddParameter(console.Player({
 	SingleTarget = true,
 	CheckImmunity = true,
-	NoSelfTarget = true,
+	NoSelfTarget = true
 }))
+
 setUserGroup:AddParameter(console.String({
-	validate.InList({ "user", "admin", "superadmin", "developer" })
+	validate.InList({"user", "admin", "superadmin", "developer"})
 }))
