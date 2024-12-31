e3b2c183c72ad06f6872acd8f759b4ac7fb78d93
TankNut <m.vlasblom@live.nl>
2024-12-31 05:14:35 +0100

    Bag of Holding

---
+++ gamemode/content/items/bags/bag_developer.lua
@@ -0,0 +1,20 @@
+DEFINE_BASECLASS("item_base_container")
+
+ITEM.Base = "base_container"
+
+ITEM.Name = "Bag of Holding"
+ITEM.Description = "This bag has an interior space considerably larger than it's outside dimensions"
+
+ITEM.Rarity = RARITY_DEVELOPER
+ITEM.Tags = {}
+
+ITEM.Model = Model("models/props_junk/garbage_bag001a.mdl")
+ITEM.Color = Color(95, 63, 127)
+
+ITEM.BaseWeight = 5
+ITEM.MaxWeight = 500
+
+-- We get to feel special and don't have to worry about how much our contents weigh
+function ITEM:GetWeight()
+	return self:GetData("BaseWeight", self.BaseWeight)
+end
