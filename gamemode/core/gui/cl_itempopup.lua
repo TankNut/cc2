fb5da6c100284f7f2dd4baa96b67e78c31676369
TankNut <m.vlasblom@live.nl>
2025-01-07 03:23:35 +0100

    Finish item popup gui

--- gamemode/core/gui/cl_itempopup.lua
+++ gamemode/core/gui/cl_itempopup.lua
@@ -8,6 +8,60 @@ function PANEL:Init()
 	self:SetDraggable(true)
 	self:SetCloseOnPause(true)
 
+	self.ModelPanel = self:Add("CCItemModelPanel")
+	self.ModelPanel:Dock(TOP)
+	self.ModelPanel:SetTall(200)
+
+	self.Buttons = self:Add("DPanel")
+	self.Buttons:DockMargin(0, 5, 0, 0)
+	self.Buttons:Dock(BOTTOM)
+	self.Buttons:SetPaintBackground(false)
+	self.Buttons:SetTall(22 * 3 + 15)
+
+	self.Right = self.Buttons:Add("DPanel")
+	self.Right:Dock(RIGHT)
+	self.Right:SetPaintBackground(false)
+	self.Right:SetWide(100)
+
+	self.DestroyButton = self.Right:Add("DButton")
+	self.DestroyButton:DockMargin(0, 5, 0, 0)
+	self.DestroyButton:Dock(BOTTOM)
+	self.DestroyButton:SetText("Destroy")
+	self.DestroyButton.DoClick = function()
+		self.Item:RunAction(lp, "Destroy")
+	end
+
+	self.DropButton = self.Right:Add("DButton")
+	self.DropButton:Dock(BOTTOM)
+	self.DropButton:DockMargin(0, 5, 0, 0)
+	self.DropButton:SetText("Drop")
+	self.DropButton.DoClick = function()
+		self.Item:RunAction(lp, "Drop")
+	end
+
+	self.ActionButton = self.Right:Add("DButton")
+	self.ActionButton:Dock(BOTTOM)
+	self.ActionButton:DockMargin(0, 5, 0, 0)
+	self.ActionButton:SetText("Actions")
+	self.ActionButton.DoClick = function()
+		self.Item:OpenActionMenu("Examine")
+	end
+
+	self.TitleLabel = self:Add("DLabel")
+	self.TitleLabel:DockMargin(0, 5, 0, 0)
+	self.TitleLabel:Dock(TOP)
+	self.TitleLabel:SetFont("CombineControl.LabelGiant")
+
+	self.Scroll = self:Add("DScrollPanel")
+	self.Scroll:Dock(FILL)
+
+	self.DescriptionLabel = self.Scroll:Add("ScribeLabel")
+
+	self.DataLabel = self.Buttons:Add("DLabel")
+	self.DataLabel:Dock(FILL)
+	self.DataLabel:SetContentAlignment(1)
+	self.DataLabel:SetTextColor(self:GetSkin().Text.Disabled)
+
 	self:MakePopup()
 	self:Center()
 end
@@ -16,7 +70,25 @@ function PANEL:Setup(item)
 	self.Item = item
 	self.Item.Panels[self] = true
 
-	self:SetTopBar(item:GetName())
+	local name = item:GetName()
+
+	self:SetTopBar(name)
+
+	self.ModelPanel:SetItem(item)
+
+	self.TitleLabel:SetText(name)
+	self.TitleLabel:SizeToContentsY()
+	self.TitleLabel:SetTextColor(item:GetRarityData().Color)
+
+	self.DescriptionLabel:SetWide(self.Scroll:GetWide() - 15)
+	self.DescriptionLabel:SetText("<font=CombineControl.LabelMedium>\n" .. item:GetDescription())
+	self.DescriptionLabel:SizeToContentsY()
+
+	self.DataLabel:SetText(string.format("Weight: %s kg\nTags: %s", item:GetWeight(), table.concat(item:GetTags(), ", ")))
+
+	self.DestroyButton:SetDisabled(not item:CanRunAction(lp, "Destroy"))
+	self.DropButton:SetDisabled(not item:CanRunAction(lp, "Drop"))
+	self.ActionButton:SetDisabled(#item:GetAvailableActions("Examine") < 1)
 end
 
 function PANEL:OnRemove()
---
+++ gamemode/core/vgui/cc_itemmodelpanel.lua
@@ -0,0 +1,120 @@
+local PANEL = {}
+
+AccessorFunc(PANEL, "FirstPerson", "FirstPerson")
+AccessorFunc(PANEL, "OrbitDistance", "OrbitDistance")
+
+function PANEL:Init()
+	self.mx = 0
+	self.my = 0
+
+	self.aLookAngle = angle_zero
+
+	self:SetOrbitDistance(100)
+end
+
+function PANEL:SetItem(item)
+	self:SetModel(item:GetModel())
+
+	local ent = self:GetEntity()
+
+	item:SetItemAppearance(ent)
+
+	self:SetOrbitDistance(ent:GetModelRadius() + 75)
+	self:InvalidateParent(true)
+	self:InvalidateLayout(true)
+
+	local angle, fov = item:GetIconCamera()
+	local ratio = self:GetWide() / self:GetTall()
+
+	self:SetLookAng(angle)
+	self:SetFOV(fov * ratio)
+end
+
+function PANEL:OnMousePressed(mousecode)
+	self:SetCursor("none")
+	self:MouseCapture(true)
+
+	self.Capturing = true
+	self.MouseCode = mousecode
+
+	self:SetFirstPerson(true)
+	self:CaptureMouse()
+end
+
+function PANEL:LayoutEntity(ent)
+	self.colColor = ent:GetColor()
+
+	local mins, maxs = ent:GetModelBounds()
+
+	self.OrbitPoint = (mins + maxs) / 2
+	self.vCamPos = self.OrbitPoint - self.aLookAngle:Forward() * self.OrbitDistance
+end
+
+function PANEL:Think()
+	if self.Capturing then
+		self:FirstPersonControls()
+	end
+end
+
+function PANEL:CaptureMouse()
+	local x, y = input.GetCursorPos()
+
+	local dx = x - self.mx
+	local dy = y - self.my
+
+	local centerx, centery = self:LocalToScreen(self:GetWide() * 0.5, self:GetTall() * 0.5)
+
+	input.SetCursorPos(centerx, centery)
+
+	self.mx = centerx
+	self.my = centery
+
+	return dx, dy
+end
+
+function PANEL:FirstPersonControls()
+	local x, y = self:CaptureMouse()
+
+	self.aLookAngle:RotateAroundAxis(self.aLookAngle:Right(), y * -0.5)
+	self.aLookAngle:RotateAroundAxis(self.aLookAngle:Up(), x * -0.5)
+end
+
+function PANEL:OnMouseWheeled(dlta)
+	local scale = self:GetFOV() / 180
+
+	self.fFOV = math.Clamp(self.fFOV + dlta * -10.0 * scale, 0.001, 179)
+end
+
+function PANEL:OnMouseReleased(mousecode)
+	self:SetCursor("arrow")
+	self:MouseCapture(false)
+
+	self.Capturing = false
+end
+
+local developer = GetConVar("developer")
+
+function PANEL:Paint(w, h)
+	surface.SetDrawColor(0, 0, 0, 70)
+	surface.DrawRect(0, 0, w, h)
+
+	render.ClearDepth()
+
+	DModelPanel.Paint(self, w, h)
+
+	if developer:GetBool() then
+		local ang = Angle(self.aLookAngle)
+
+		ang:Normalize()
+
+		local _, y = draw.SimpleText(string.format("%.2f %.2f %.2f", ang.p, ang.y, ang.r), "BudgetLabel", 2)
+		local ratio = self:GetWide() / self:GetTall()
+
+		draw.SimpleText(string.format("%.2f", self.fFOV / ratio), "BudgetLabel", 2, y)
+	end
+
+	surface.SetDrawColor(self:GetSkin().Colors.Border)
+	surface.DrawOutlinedRect(0, 0, w, h)
+end
+
+derma.DefineControl("CCItemModelPanel", "", PANEL, "DModelPanel")
