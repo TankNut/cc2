9bb85f849c8d5b1d95dc6790e48c2404a9818f79
TankNut <m.vlasblom@live.nl>
2025-01-12 06:14:19 +0100

    msg_generic can overwrite it's color

--- gamemode/content/chat/msg_generic.lua
+++ gamemode/content/chat/msg_generic.lua
@@ -3,7 +3,7 @@ CLASS.Name = "GENERIC"
 CLASS.Color = Color(200, 200, 200)
 
 if CLIENT then
-	function CLASS:OnReceive(data, colors)
-		return string.format("<c=%s>%s", self.Color, data.Text)
+	function CLASS:OnReceive(data)
+		return string.format("<c=%s>%s", data.Color or self.Color, data.Text)
 	end
 end
