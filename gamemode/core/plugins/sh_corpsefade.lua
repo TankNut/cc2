673b415f12ef335c42df7c26d157f86bccadea28
TankNut <m.vlasblom@live.nl>
2025-02-11 01:41:04 +0100

    Corpsefade plugin

---
+++ gamemode/core/plugins/sh_corpsefade.lua
@@ -0,0 +1,25 @@
+Config.Fallback("CorpseFade", 30)
+
+local function fade(ent)
+	timer.Simple(Config.Get("CorpseFade"), function()
+		if not IsValid(ent) then
+			return
+		end
+
+		if CLIENT then
+			ent:SetSaveValue("m_bFadingOut", true)
+		else
+			ent:Fire("FadeAndRemove")
+		end
+	end)
+end
+
+if CLIENT then
+	hook.Add("CreateClientsideRagdoll", "plugins.corpsefade", function(ent, ragdoll)
+		fade(ragdoll)
+	end)
+else
+	hook.Add("CreateEntityRagdoll", "plugins.corpsefade", function(ent, ragdoll)
+		fade(ragdoll)
+	end)
+end
