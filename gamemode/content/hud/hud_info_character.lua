565742537dc3729f19ac4c49627ae0b342da2515
TankNut <m.vlasblom@live.nl>
2025-02-25 00:09:39 +0100

    Rework the character hud to rely on scribe caching

--- gamemode/content/hud/hud_info_character.lua
+++ gamemode/content/hud/hud_info_character.lua
@@ -5,50 +5,25 @@ HUD.Setting = "CharacterInfo"
 
 HUD.DrawOrder = 0
 
-function HUD:Initialize()
-	self:UpdateInfo()
-
-	hook.Add("OnVisibleRPNameChanged", self, self.OnVisibleRPNameChanged)
-
-	self.Dark = Color("cc_dark")
-end
-
-function HUD:OnVisibleRPNameChanged(ply, _, new)
-	if ply == lp then
-		self:UpdateInfo(new)
-	end
-end
-
-function HUD:UpdateInfo(name)
-	self.LastName = name or lp:VisibleRPName()
-	self.LastTeam = lp:Team()
-
-	self.NameScribe = scribe.Parse("<giant><ol><c=cc_normal>" .. self.LastName)
-	self.TeamScribe = scribe.Parse("<giant><ol><c=cc_normal>" .. team.GetName(self.LastTeam))
-end
-
-function HUD:Think()
-	if lp:Team() != self.LastTeam then
-		self:UpdateInfo()
-	end
-end
-
 function HUD:Paint(w, h)
 	local offset = 20
 	local margin = 2
 
 	local x, y = offset, h - offset
 
-	local scribeW = math.max(self.NameScribe:GetWide(), self.TeamScribe:GetWide())
-	local scribeH = self.NameScribe:GetTall() + self.TeamScribe:GetTall()
+	local nameScribe = scribe.Parse("<giant><ol><c=cc_normal>" .. lp:VisibleRPName())
+	local teamScribe = scribe.Parse("<giant><ol><c=cc_normal>" .. team.GetName(lp:Team()))
+
+	local scribeW = math.max(nameScribe:GetWide(), teamScribe:GetWide())
+	local scribeH = nameScribe:GetTall() + teamScribe:GetTall()
 
 	local boxW = math.max(scribeW + margin * 2, 220)
 	local boxH = scribeH + margin * 2
 
 	self:DrawAlignedRect(x, y, boxW, boxH, Color("cc_fill_dark", 200), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
 
-	self.NameScribe:Draw(x + boxW - margin, y - boxH + margin, 1, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
-	self.TeamScribe:Draw(x + boxW - margin, y - margin, 1, TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM)
+	nameScribe:Draw(x + boxW - margin, y - boxH + margin, 1, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
+	teamScribe:Draw(x + boxW - margin, y - margin, 1, TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM)
 
 	self:SetCache("LOffset", y - boxH)
 end
