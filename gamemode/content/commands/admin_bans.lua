4b3ee86cb09a7c90dacbcd39fc9f79f094052de9
TankNut <m.vlasblom@live.nl>
2025-03-02 02:49:13 +0100

    Ban commands

---
+++ gamemode/content/commands/admin_bans.lua
@@ -0,0 +1,29 @@
+local ban = console.AddCommand("rpa_ban", function(ply, steamID, length, reason)
+	Access.AddBan(steamID, ply, length, reason)
+end)
+
+ban:SetCategory("Bans")
+ban:SetDescription("Bans a player for a specified time, a length of 0 will ban them permanently")
+ban:SetExecutionContext(console.Server)
+ban:SetAccess(console.IsAdmin)
+
+ban:AddParameter(console.SteamID({
+	StrictImmunity = true,
+	NoSelfTarget = true
+}))
+
+ban:AddParameter(console.Duration({}, "length"))
+ban:AddOptional(console.String({
+	Max = 256
+}), nil, "No reason specified")
+
+local unban = console.AddCommand("rpa_unban", function(ply, steamID)
+	Access.LiftBan(steamID)
+end)
+
+unban:SetCategory("Bans")
+unban:SetDescription("Unbans a player, does nothing if they're not")
+unban:SetExecutionContext(console.Server)
+unban:SetAccess(console.IsAdmin)
+
+unban:AddParameter(console.SteamID())
