92f515528f6c871c59c0f97934439aa44f9b0392
Drewerth <125228928+drewerth@users.noreply.github.com>
2025-03-16 02:03:35 -0400

    Add admin context menu buttons.

---
+++ entities/entities/cc_utility_button.lua
@@ -0,0 +1,148 @@
+DEFINE_BASECLASS("cc_worldent_picker")
+AddCSLuaFile()
+
+ENT.Base = "cc_worldent_picker"
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+ENT.PrintName = "Quick Button"
+ENT.CCMainCategory = "World Entities"
+ENT.CCSubCategory = "Utilities"
+
+ENT.Spawnable = false
+ENT.AdminOnly = true
+
+ENT.Color = Color(200, 60, 255)
+
+EntityCache.Add("quickbuttons", function(ent) return ent:IsType("cc_utility_button") end)
+
+local validation = {
+	validate.Max(32)
+}
+
+ENT.Actions = {}
+ENT.Actions.SetButton = {
+	Name = "Set Button ID",
+
+	EditMode = true,
+	Interaction = true,
+
+	CanRun = function(self, ply) return not self:IsSaved() end,
+	Validate = function(self, ply, name)
+		return validate.Value(name, validation)
+	end,
+	Client = function(self, ply)
+		return true, GUI.Open("Input", "string", "Set Button ID", {
+			Default = self:GetButtonID(),
+			Validate = validation,
+			Name = "Button ID"
+		})
+	end,
+	Callback = function(self, ply, id)
+		self:SetButtonID(id)
+	end
+}
+
+Action.Add("QuickButton", {
+	Name = "Admin Utilities/Quick Button",
+
+	Self = true,
+	Context = "Admin",
+
+	CanRun = FindMetaTable("Player").IsAdmin,
+	SubOptions = function(self, ply)
+		local options = {}
+
+		table.insert(options, {
+			Name = "Create Quick Button...",
+			Value = nil
+		})
+
+		for ent in pairs(EntityCache.Get("quickbuttons")) do
+			if not ent:IsSaved() then
+				continue
+			end
+
+			table.insert(options, {
+				Name = ent:GetButtonID(),
+				Value = ent
+			})
+		end
+		return options
+	end,
+
+	Validate = function(self, ply, ent)
+		return IsValid(ent) and ent:IsType("cc_utility_button") and ent:IsSaved()
+	end,
+	Client = function(self, ply, ent)
+		if not ent then
+			ply:ConCommand("gm_spawnsent cc_utility_button")
+
+			return false
+		end
+
+		return true, ent
+	end,
+	Callback = function(self, ply, ent)
+		ent:GetPickedEntity():Input("Use", ply, ent)
+	end
+})
+
+function ENT:Initialize()
+	BaseClass.Initialize(self)
+
+	self:AddEFlags(EFL_FORCE_CHECK_TRANSMIT)
+end
+
+function ENT:SetupDataTables()
+	BaseClass.SetupDataTables(self)
+
+	self:NetworkVar("String", "ButtonID")
+end
+
+function ENT:CanSave()
+	if not BaseClass.CanSave(self) then
+		return false
+	end
+
+	for ent in pairs(EntityCache.Get("quickbuttons")) do
+		if self != ent and self:GetButtonID() == ent:GetButtonID() then
+			return false
+		end
+	end
+
+	return #self:GetButtonID() > 0
+end
+
+if CLIENT then
+	function ENT:GetLabel()
+		local name = self:GetButtonID()
+
+		if #name > 0 then
+			return "Button: " .. name
+		else
+			return "Button: *INVALID*"
+		end
+	end
+
+	function ENT:DrawTranslucent()
+		BaseClass.DrawTranslucent(self)
+
+		if self:ShouldDraw() then
+			render.DrawWorldText(self:LocalToWorld(Vector(0, 0, self:GetModelRadius() + 8)), self:GetLabel())
+		end
+	end
+else
+	function ENT:UpdateTransmitState()
+		return TRANSMIT_ALWAYS
+	end
+
+	function ENT:GetSaveData()
+		return {
+			ButtonID = self:GetButtonID()
+		}
+	end
+
+	function ENT:LoadSaveData(data)
+		self:SetButtonID(data.ButtonID)
+	end
+end
