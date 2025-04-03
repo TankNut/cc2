4e2e045100e02f0d1dbac7b4d1e283f0fc808554
TankNut <m.vlasblom@live.nl>
2025-04-04 01:11:40 +0200

    Button hud labels are distance based

--- gamemode/content/hud/hud_world_button.lua
+++ gamemode/content/hud/hud_world_button.lua
@@ -2,47 +2,23 @@ HUD.Name = "Button Labels"
 
 HUD.Setting = "ButtonLabels"
 
-function HUD:Initialize()
-	self.Cache = {}
-end
-
-function HUD:Think()
-	local ct = CurTime()
-	local ft = FrameTime()
+function HUD:PaintBackground(w, h)
+	local eye = EyePos()
 
 	for button in pairs(Buttons.List) do
-		if not self.Cache[button] then
-			self.Cache[button] = {
-				Alpha = 0,
-				FadeTime = ct,
-			}
-		end
-
-		local cache = self.Cache[button]
-
-		if lp:CanSee(button, Config.Get("InteractRange")) then
-			cache.Alpha = math.min(cache.Alpha + ft, 1)
-			cache.FadeTime = ct + 0.05
-		elseif cache.FadeTime < ct then
-			cache.Alpha = math.max(cache.Alpha - ft, 0)
-		end
-	end
-end
-
-function HUD:PaintBackground(w, h)
-	for button, cache in pairs(self.Cache) do
-		if not IsValid(button) then
-			self.Cache[button] = nil
-
-			continue
-		end
-
 		if button:IsDormant() or #button:ButtonName() == 0 then
 			continue
 		end
 
+		local pos = button:WorldSpaceCenter()
+		local alpha = math.ClampedRemap(eye:Distance(pos), MAX_USE_DISTANCE, MAX_USE_DISTANCE * 1.5, 1, 0)
+
+		if alpha <= 0 then
+			continue
+		end
+
 		self:AddWorldLabel(button:WorldSpaceCenter(), {{
-			scribe.Parse(string.format("<f=BudgetLabel><ol>%s", button:ButtonName())), cache.Alpha
+			scribe.Parse(string.format("<f=BudgetLabel><ol>%s", button:ButtonName())), alpha
 		}})
 	end
 end
