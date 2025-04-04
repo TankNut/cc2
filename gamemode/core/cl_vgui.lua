0eb547e7bd85757f692a4dfa47b67360ba42b42d
TankNut <m.vlasblom@live.nl>
2025-04-05 01:45:08 +0200

    Fix close buttons not appearing on cc_frame

--- gamemode/core/cl_vgui.lua
+++ gamemode/core/cl_vgui.lua
@@ -1,6 +1,8 @@
 local PANEL = FindMetaTable("Panel")
 
 function PANEL:SetCloseOnPause()
+	self.m_bCloseOnPause = true
+
 	hook.Add("OnPauseMenuShow", self, function()
 		if vgui.FocusedHasParent(self) then
 			self:Close()
