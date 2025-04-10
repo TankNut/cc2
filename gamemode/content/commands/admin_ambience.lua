2a15c105160ef7872b94054e343a080fd3b080c9
Drewerth <125228928+drewerth@users.noreply.github.com>
2025-04-10 13:04:05 -0400

    Area sounds nicer than priority.

--- gamemode/content/commands/admin_ambience.lua
+++ gamemode/content/commands/admin_ambience.lua
@@ -15,15 +15,15 @@ if CLIENT then
 	netstream.Hook("StopEffect", Ambience.StopEffect)
 end
 
-local priorityMapping = {
+local areaMapping = {
 	["global"] = {
-		Priority = AMBIENCE_GLOBAL,
+		area = AMBIENCE_GLOBAL,
 		GetTargets = function()
 			return player.GetAll()
 		end
 	},
 	["local"] = {
-		Priority = AMBIENCE_LOCAL,
+		area = AMBIENCE_LOCAL,
 		GetTargets = function(ply)
 			local targets = {ply}
 
@@ -57,27 +57,27 @@ local killAmbience = console.AddCommand("rpa_killambience", function(ply)
 end)
 
 killAmbience:SetCategory("Ambience")
-killAmbience:SetDescription("Stops all custom music and effects from playing, regardless of priority")
+killAmbience:SetDescription("Stops all custom music and effects from playing, regardless of area")
 killAmbience:SetExecutionContext(console.Server)
 killAmbience:SetAccess(console.IsAdmin)
 
 local playMusic = console.AddCommand("rpa_playmusic", function(ply, level, path, volume)
 	Log.Write("admin_playmusic", ply, level, path, volume)
 
-	local config = priorityMapping[level]
+	local config = areaMapping[level]
 
-	netstream.Send(config.GetTargets(ply), "PlayMusic", config.Priority, path, volume, ply:Nick())
+	netstream.Send(config.GetTargets(ply), "PlayMusic", config.area, path, volume, ply:Nick())
 end)
 
 playMusic:SetCategory("Ambience")
-playMusic:SetDescription("Plays a music track with the given priority")
+playMusic:SetDescription("Plays a music track with the given area")
 playMusic:SetExecutionContext(console.Server)
 playMusic:SetAccess(console.IsAdmin)
 playMusic:SetNoConsole()
 
 playMusic:AddParameter(console.String({
-	validate.InList(table.GetKeys(priorityMapping))
-}, "priority"))
+	validate.InList(table.GetKeys(areaMapping))
+}, "area"))
 
 playMusic:AddParameter(console.String({}, "path"))
 
@@ -87,37 +87,37 @@ playMusic:AddOptional(console.Number({
 }, "volume"), 1)
 
 local stopMusic = console.AddCommand("rpa_stopmusic", function(ply, level)
-	local config = priorityMapping[level]
+	local config = areaMapping[level]
 
-	netstream.Send(config.GetTargets(ply), "StopMusic", config.Priority)
+	netstream.Send(config.GetTargets(ply), "StopMusic", config.area)
 end)
 
 stopMusic:SetCategory("Ambience")
-stopMusic:SetDescription("Stops a music track with the given priority from playing")
+stopMusic:SetDescription("Stops a music track with the given area from playing")
 stopMusic:SetExecutionContext(console.Server)
 stopMusic:SetAccess(console.IsAdmin)
 
 stopMusic:AddParameter(console.String({
-	validate.InList(table.GetKeys(priorityMapping))
-}, "priority"))
+	validate.InList(table.GetKeys(areaMapping))
+}, "area"))
 
 local playEffect = console.AddCommand("rpa_playeffect", function(ply, level, path, volume)
 	Log.Write("admin_playeffect", ply, level, path, volume)
 
-	local config = priorityMapping[level]
+	local config = areaMapping[level]
 
-	netstream.Send(config.GetTargets(ply), "PlayEffect", config.Priority, path, volume, ply:Nick())
+	netstream.Send(config.GetTargets(ply), "PlayEffect", config.area, path, volume, ply:Nick())
 end)
 
 playEffect:SetCategory("Ambience")
-playEffect:SetDescription("Plays an effect with the given priority")
+playEffect:SetDescription("Plays an effect with the given area")
 playEffect:SetExecutionContext(console.Server)
 playEffect:SetAccess(console.IsAdmin)
 playEffect:SetNoConsole()
 
 playEffect:AddParameter(console.String({
-	validate.InList(table.GetKeys(priorityMapping))
-}, "priority"))
+	validate.InList(table.GetKeys(areaMapping))
+}, "area"))
 
 playEffect:AddParameter(console.String({}, "path"))
 
@@ -127,16 +127,16 @@ playEffect:AddOptional(console.Number({
 }, "volume"), 1)
 
 local stopEffect = console.AddCommand("rpa_stopeffect", function(ply, level)
-	local config = priorityMapping[level]
+	local config = areaMapping[level]
 
-	netstream.Send(config.GetTargets(ply), "StopEffect", config.Priority)
+	netstream.Send(config.GetTargets(ply), "StopEffect", config.area)
 end)
 
 stopEffect:SetCategory("Ambience")
-stopEffect:SetDescription("Stops an effect with the given priority from playing")
+stopEffect:SetDescription("Stops an effect with the given area from playing")
 stopEffect:SetExecutionContext(console.Server)
 stopEffect:SetAccess(console.IsAdmin)
 
 stopEffect:AddParameter(console.String({
-	validate.InList(table.GetKeys(priorityMapping))
-}, "priority"))
+	validate.InList(table.GetKeys(areaMapping))
+}, "area"))
