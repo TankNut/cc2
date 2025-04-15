f0b4ac0ef34513cd298ff559fca9ca3694dbcc07
TankNut <m.vlasblom@live.nl>
2025-04-15 23:46:32 +0200

    Move some key stuff to base_key

--- gamemode/content/items/_base/base_key.lua
+++ gamemode/content/items/_base/base_key.lua
@@ -1,8 +1,14 @@
 ITEM.Internal = true
 
+ITEM.Rarity = RARITY_COMMON
 ITEM.Category = "Key"
 
 ITEM.Model = Model("models/gibs/metal_gib4.mdl")
+
+ITEM.Weight = 0.1
+
+ITEM.IconAngle = Angle(50, -35, -10)
+ITEM.IconFOV = 6
 
 ITEM.KeyType = KEY_BOTH
 ITEM.KeyID   = ""
--- gamemode/content/items/dev/key_developer.lua
+++ gamemode/content/items/dev/key_developer.lua
@@ -5,10 +5,6 @@ ITEM.Description = "A key that somehow fits every lock"
 
 ITEM.Rarity = RARITY_DEVELOPER
 
-ITEM.Weight = 0.1
 
 ITEM.KeyType = KEY_BOTH
 ITEM.KeyID   = "*"
-
-ITEM.IconAngle = Angle(50, -35, -10)
-ITEM.IconFOV = 6
