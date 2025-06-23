82aa94c8fd643776017584f9160a54b77172c401
TankNut <m.vlasblom@live.nl>
2025-06-24 01:20:54 +0200

    Spartan prosthetic arm

---
+++ gamemode/content/items/unsc/spartan_arm.lua
@@ -0,0 +1,29 @@
+ITEM.Name           = "MJOLNIR Prosthetic Arm"
+
+ITEM.Rarity         = RARITY_LEGENDARY
+ITEM.Category       = "Spartan"
+
+ITEM.Model       	= Model("models/rena_haloreach/crate_packing.mdl")
+
+ITEM.IconAngle   	= Angle(30, 27, 0)
+ITEM.IconFOV     	= 35
+
+ITEM.Weight         = 2
+
+ITEM.EquipmentSlots = {"spartan_arm"}
+
+if SERVER then
+	function ITEM:GetModelData(ply, clothing)
+		if not self:IsEquipped() then
+			return
+		end
+
+		return {
+			_base = {
+				Bodygroups = {
+					Arm = 1
+				}
+			}
+		}
+	end
+end
