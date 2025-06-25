c762f27f4f93ec4590d1536d7cb0b2cb6688cdbb
TankNut <m.vlasblom@live.nl>
2025-06-25 04:19:32 +0200

    Disable broken legs for now

--- gamemode/content/buffs/buff_broken_legs.lua
+++ gamemode/content/buffs/buff_broken_legs.lua
@@ -42,26 +42,26 @@ function BUFF:Move(mv)
 	mv:LimitSpeed(Lerp(fraction, crouchSpeed, self.Player:GetRunSpeed()))
 end
 
-if SERVER then
-	hook.Add("OnTakeFallDamage", "broken_legs", function(ply, damage)
-		ply:AddBuff("broken_legs", {
-			Duration = math.min(damage, 20)
-		})
-	end)
+-- if SERVER then
+-- 	hook.Add("OnTakeFallDamage", "broken_legs", function(ply, damage)
+-- 		ply:AddBuff("broken_legs", {
+-- 			Duration = math.min(damage, 20)
+-- 		})
+-- 	end)
 
-	hook.Add("PostEntityTakeDamage", "broken_legs", function(ply, dmg, took)
-		if not ply:IsPlayer() or not took or not bit.Check(dmg:GetDamageType(), DMG_BULLET) then
-			return
-		end
+-- 	hook.Add("PostEntityTakeDamage", "broken_legs", function(ply, dmg, took)
+-- 		if not ply:IsPlayer() or not took or not bit.Check(dmg:GetDamageType(), DMG_BULLET) then
+-- 			return
+-- 		end
 
-		local hitgroup = ply:LastHitGroup()
+-- 		local hitgroup = ply:LastHitGroup()
 
-		if hitgroup != HITGROUP_LEFTLEG and hitgroup != HITGROUP_RIGHTLEG then
-			return
-		end
+-- 		if hitgroup != HITGROUP_LEFTLEG and hitgroup != HITGROUP_RIGHTLEG then
+-- 			return
+-- 		end
 
-		ply:AddBuff("broken_legs", {
-			Duration = math.min(dmg:GetDamage(), 10)
-		})
-	end)
-end
+-- 		ply:AddBuff("broken_legs", {
+-- 			Duration = math.min(dmg:GetDamage(), 10)
+-- 		})
+-- 	end)
+-- end
