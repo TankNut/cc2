6dc9b32c7a99a2f0d42811e58a562b444dff14ec
TankNut <m.vlasblom@live.nl>
2025-11-08 02:54:30 +0100

    Projectiles more accurately track your aimpoint

--- entities/weapons/weapon_cc_base_gun/attacks/sh_projectile.lua
+++ entities/weapons/weapon_cc_base_gun/attacks/sh_projectile.lua
@@ -1,20 +1,34 @@
 AddCSLuaFile()
 
+function SWEP:GetProjectileSetup(owner, offset, offsetAng)
+	local spread = self:GetSpread()
+	spread = AngleRand(-spread, spread)
+	spread.r = 0
+
+	local pos, ang = LocalToWorld(vector_origin, offsetAng + spread, owner:GetShootPos(), self:GetShootDir():Angle())
+	local tr = util.TraceLine({
+		start = pos,
+		endpos = pos + (ang:Forward() * 2000),
+		filter = owner,
+		mask = MASK_SOLID
+	})
+
+	pos = pos + (ang:Forward() * offset.x) + (ang:Right() * -offset.y) + (ang:Up() * offset.z)
+
+	if tr.Fraction > 0.1 then
+		ang = (tr.HitPos - pos):Angle()
+	end
+
+	return pos, ang
+end
+
 function SWEP:FireProjectile(owner)
 	if CLIENT then
 		return
 	end
 
 	local ent = ents.Create(self.Stats.Class)
-
-	local spread = self:GetSpread()
-	spread = AngleRand(-spread, spread)
-	spread.r = 0
-
-	local pos = self.Stats.Offset or vector_origin
-	local ang = self.Stats.Angle or angle_zero
-
-	pos, ang = LocalToWorld(pos, ang + spread, owner:GetShootPos(), self:GetShootDir():Angle())
+	local pos, ang = self:GetProjectileSetup(owner, self.Stats.Offset or vector_origin, self.Stats.Angle or angle_zero)
 
 	ent:SetPos(pos)
 	ent:SetAngles(ang)
