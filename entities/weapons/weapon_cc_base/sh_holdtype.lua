9a4e99af94a3aec6023065ba250e403c2ca1fa09
TankNut <m.vlasblom@live.nl>
2025-12-27 01:45:19 +0100

    Shotgun holdtype uses AR2 jumping animations

--- entities/weapons/weapon_cc_base/sh_holdtype.lua
+++ entities/weapons/weapon_cc_base/sh_holdtype.lua
@@ -53,7 +53,7 @@ holdTypes.shotgun[ACT_MP_CROUCH_IDLE] = ACT_HL2MP_IDLE_CROUCH_AR2
 holdTypes.shotgun[ACT_MP_CROUCHWALK] = ACT_HL2MP_WALK_CROUCH_AR2
 holdTypes.shotgun[ACT_MP_RELOAD_STAND] = ACT_HL2MP_GESTURE_RELOAD_AR2
 holdTypes.shotgun[ACT_MP_RELOAD_CROUCH] = ACT_HL2MP_GESTURE_RELOAD_AR2
-
+holdTypes.shotgun[ACT_MP_JUMP] = ACT_HL2MP_JUMP_AR2
 
 holdTypes.heavy = table.Copy(holdTypes.crossbow)
 holdTypes.heavy[ACT_MP_CROUCH_IDLE] = ACT_HL2MP_IDLE_CROUCH_PHYSGUN
