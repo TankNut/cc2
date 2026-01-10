5fe3f649912c0e189e40d0177e94e837c3f4340a
TankNut <m.vlasblom@live.nl>
2026-01-10 05:46:52 +0100

    Add some documentation to sh_balance.lua

--- gamemode/config/sh_balance.lua
+++ gamemode/config/sh_balance.lua
@@ -1,9 +1,12 @@
+-- How much damage is left over per 1000 source units
 DMG_FALLOFF_SHOTGUN = 0.8
 DMG_FALLOFF_SMG     = 0.9
 DMG_FALLOFF_RIFLE   = 0.94
 DMG_FALLOFF_SNIPER  = 0.98
 DMG_FALLOFF_NONE    = 1
 
+-- See this as the mechanical accuracy of a weapon, it's combined with SWEP.Settings.Range to get the final spread values
+-- Values are for hip and aimed shots respectively
 ACCURACY_POOR    = {16, 8}
 ACCURACY_AVERAGE = {14, 6}
 ACCURACY_GOOD    = {12, 4}
