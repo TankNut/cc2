df3d900d9402eb5c54a4633dff75002411fffc7c
TankNut <m.vlasblom@live.nl>
2026-01-21 01:39:09 +0100

    Improve attachment shadow handling

--- entities/entities/cc_attachment.lua
+++ entities/entities/cc_attachment.lua
@@ -4,19 +4,24 @@ ENT.Type = "anim"
 
 if CLIENT then
 	function ENT:Draw(flags)
+		local shouldDraw = true
+
 		-- We're a honest to god entity, so our parent is too (nothing vgui related), so we do some extra checks to see if we should bother drawing
 		if self:EntIndex() != -1 then
 			local parent = self:GetParent()
 
 			if parent:GetNoDraw() or not parent:Alive() then
-				return
-			end
-
-			if parent == lp and not parent:ShouldDrawLocalPlayer() then
-				return
+				shouldDraw = false
+			elseif parent == lp and not parent:ShouldDrawLocalPlayer() then
+				shouldDraw = false
 			end
 		end
 
-		self:DrawModel(flags)
+		if shouldDraw then
+			self:DrawModel(flags)
+			self:CreateShadow()
+		else
+			self:DestroyShadow()
+		end
 	end
 end
